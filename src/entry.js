95c073cd12f039c580472bd952bdd99f79c63e40
Nathan Leung <18nleung@gmail.com>
2016-10-25 13:44:28 +0000

    initial site commit

---
+++ src/entry.js
@@ -0,0 +1,20 @@
+import { tokenize, countElements, getMass } from '../../src/index';
+ 
+/**
+ * Get elements data
+ */
+
+document.getElementById('atoms').addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const resultEl = document.getElementById('result');
+  const tokens = tokenize(e.target.formula.value);
+  const counts = countElements(tokens);
+  
+	resultEl.innerHTML =
+	  `Counts: ${JSON.stringify(counts)}\n` +
+	  `Mass: ${getMass(counts)}`
+}
+
+
